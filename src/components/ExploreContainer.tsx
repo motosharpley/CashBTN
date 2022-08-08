352ef4b1ab4b36b6d649d63b7319f8a6d27dc0fb
Bryan Sharpley <motosharpley@gmail.com>
2022-08-08 13:16:00 -0700

    firebase setup

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,21 @@
 import './ExploreContainer.css';
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>Ready to accept crypto?</strong>
+      <p>
+        Start with CashBTN{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          Crypto UI Components
+        </a>
+      </p>
     </div>
   );
 };
