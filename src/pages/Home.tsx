87a880e1c18f44f36ef77c5df4385a7602fe4b6a
Bryan Sharpley <motosharpley@gmail.com>
2022-08-08 17:52:54 -0700

    boilerplate cleanup and stub out landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,38 @@
 import {
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>CashBTN</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">CashBTN</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <div className="content-container">
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle slot="start">CashBTN</IonTitle>
+              <IonButtons slot="start">
+                <IonButton>Home</IonButton>
+                <IonButton>Plugins</IonButton>
+                <IonButton>Developers</IonButton>
+              </IonButtons>
+              <IonButton color={'medium'}>Get Started</IonButton>
+            </IonToolbar>
+          </IonHeader>
+          <div className="cta">
+            <h3>Crypto Payments Made Easy</h3>
+            <p>Simple Plugins for Any Platform</p>
+            <IonButton color={'tertiary'}>
+              Start Accepting Crypto Today!
+            </IonButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
