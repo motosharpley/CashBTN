ccdc60f39cacca3cc6979bdc173be4065e21c40b
Bryan Sharpley <motosharpley@gmail.com>
2022-08-09 22:16:58 -0700

    make nav transparent add grid for plugin cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,13 @@ import {
   IonButton,
   IonButtons,
   IonCard,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
   IonInput,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
@@ -17,18 +20,16 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-        <div className="lander-container">
-          <IonHeader id="navHeader">
-            <IonToolbar>
-              <IonTitle slot="start">CashBTN</IonTitle>
-              <IonButtons slot="start">
-                <IonButton>Home</IonButton>
-                <IonButton>Plugins</IonButton>
-                <IonButton>Developers</IonButton>
-              </IonButtons>
-              <IonButton color={'medium'}>Get Started</IonButton>
-            </IonToolbar>
-          </IonHeader>
+        <div className="lander-container" id="Home">
+          <IonToolbar color={'medium'} id="navHeader">
+            <IonTitle slot="start">CashBTN</IonTitle>
+            <IonButtons slot="start">
+              <IonButton href="#home">Home</IonButton>
+              <IonButton href="#plugins">Plugins</IonButton>
+              <IonButton href="#developers">Developers</IonButton>
+            </IonButtons>
+            <IonButton color={'dark'}>Get Started</IonButton>
+          </IonToolbar>
           <div className="cta">
             <h3>Crypto Payments Made Easy</h3>
             <p>Simple Plugins for Any Platform</p>
@@ -38,29 +39,40 @@ const Home: React.FC = () => {
           </div>
         </div>
         {/* Plugins Section Start */}
-        <div className="plugins-container">
+        <div className="plugins-container" id="plugins">
           <IonTitle>Ready to Collect</IonTitle>
           <p>How Will You Charge?</p>
-          <IonCard className="tips-card">
-            <h3>TipsBTN</h3>
-            <p>0.0001eth</p>
-            <p>Simple Configurable Tip Button</p>
-          </IonCard>
-          <IonCard className="tips-card">
-            <h3>CashBTN</h3>
-            <p>0.0001eth</p>
-            <p>The works all your crypto payment needs</p>
-          </IonCard>{' '}
-          <IonCard className="tips-card">
-            <h3>SubBTN</h3>
-            <p>0.0001eth</p>
-            <p>
-              Subscription based sign up button for recurring crypto payments
-            </p>
-          </IonCard>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonCard className="tips-card">
+                  <h3>TipsBTN</h3>
+                  <p>0.0001eth</p>
+                  <p>Simple Configurable Tip Button</p>
+                </IonCard>
+              </IonCol>
+              <IonCol>
+                <IonCard className="tips-card">
+                  <h3>CashBTN</h3>
+                  <p>0.0001eth</p>
+                  <p>The works all your crypto payment needs</p>
+                </IonCard>
+              </IonCol>
+              <IonCol>
+                <IonCard className="tips-card">
+                  <h3>SubBTN</h3>
+                  <p>0.0001eth</p>
+                  <p>
+                    Subscription based sign up button for recurring crypto
+                    payments
+                  </p>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
         </div>
         {/* Plugins Section End */}
-        <div className="subs-container">
+        <div className="subs-container" id="developers">
           <IonCard>
             <h3>Subscribe to get Promos Insider Early Access Features</h3>
             <p>
